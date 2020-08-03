4f87cf47599fdd79ee25894318ce068d539443f5
Sean-PC <shahramtaba@gmail.com>
2020-08-02 19:24:38 -0600

    Progress #4

--- src/com/seantaba/TerminalController.java
+++ src/com/seantaba/TerminalController.java
@@ -1,4 +1,21 @@
 package com.seantaba;
 
+import com.fazecast.jSerialComm.SerialPort;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 public class TerminalController {
+    @FXML
+    private TextArea textArea;
+    private Thread terminalTaskThread;
+
+    public void runTerminalTask()
+    {
+        terminalTaskThread = new Thread(new TerminalTask((SerialPort) textArea.getScene().getUserData(), textArea));
+        terminalTaskThread.start();
+    }
+    public void stopTerminalTask()
+    {
+        terminalTaskThread.interrupt();
+    }
 }
